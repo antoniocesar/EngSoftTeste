ab43c1512ad0c718e1e4e7206322b33c0abfc84a
antoniocesar <antoniocesar01@gmail.com>
2015-08-26 22:00:07 -0300

    First Release
    Teste Do Git

---
+++ TesteNF/ClassLibrary1/TributacaoHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TesteNF.Utils
+{
+    public static class TributacaoHelper
+    {
+        public static double CalcularPIS(double valorBase)
+        {
+            return 0;
+        }
+        public static double CalcularCOFINS(double valorBase)
+        {
+            return 0;
+        }
+        public static double CalcularIRPJ(double valorBase)
+        {
+            return 0;
+        }
+        public static double CalcularCSLL(double valorBase)
+        {
+            return 0;
+        }
+    }
+}
+
+
